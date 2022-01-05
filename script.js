0c2a857897a192be71a2d4e00b37d8d18072b60c
George Ciurca <george.ciurca@gmail.com>
2022-01-05 09:09:57 +0000

    js course

---
+++ script.js
@@ -0,0 +1,7 @@
+ let clickBtn = document.getElementById('increment-btn');
+
+ clickBtn.addEventListener('click', increment());
+
+function increment () {
+    console.log("the button was clicked");
+};
