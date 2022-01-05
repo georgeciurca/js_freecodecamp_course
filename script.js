b006fb54d0c29392c9793f33993b2c799a85c68d
George Ciurca <george.ciurca@gmail.com>
2022-01-05 09:40:43 +0000

    learning JS

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
- let clickBtn = document.getElementById('increment-btn');
+// const myAge = 45;
+// const humanDogRatio = 7;
+// const myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
 
- clickBtn.addEventListener('click', increment());
+// let bonusPoints = 50;
+// bonusPoints = bonusPoints + 50;
+// bonusPoints = bonusPoints - 75;
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints);
 
-function increment () {
-    console.log("the button was clicked");
+/*   let clickBtn = document.getElementById('increment-btn');
+
+  clickBtn.addEventListener('click', increment()); */
+
+/* function countdown (){
+    console.log(5);
+    console.log(4);
+    console.log(3);
+    console.log(2);
+    console.log(1);
+}
+countdown();  */
+
+/* function log42() {
+    console.log(42);
 };
+
+log42(); */
+
+ function increment () {
+     console.log("the button was clicked");
+ };
+
+
